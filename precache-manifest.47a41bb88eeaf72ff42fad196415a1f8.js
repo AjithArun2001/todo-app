bedde1ca34ee8fe848187087a35257e93ecc40f8
AjithArun2001 <ajitharu201@gmail.com>
2020-08-20 16:31:40 +0530

    Updates

---
+++ precache-manifest.47a41bb88eeaf72ff42fad196415a1f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8f38c98cc9c0d2855f82c839aa2fe0",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "f473360b3268c0503a9f",
+    "url": "/todo-app/static/css/main.e0c0323b.chunk.css"
+  },
+  {
+    "revision": "76e68b5609ee7a5b78e7",
+    "url": "/todo-app/static/js/2.4adbfaa9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app/static/js/2.4adbfaa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f473360b3268c0503a9f",
+    "url": "/todo-app/static/js/main.d97b2c71.chunk.js"
+  },
+  {
+    "revision": "bb0f6ae7bebc13485974",
+    "url": "/todo-app/static/js/runtime-main.cbbaed1e.js"
+  }
+]);
